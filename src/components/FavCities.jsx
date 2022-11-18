fa31efd93fa01476219d55db5d75eb29d1ea8442
Samnan <samnansiddique@yandex.com>
2022-11-18 23:31:46 +0530

    First commit

---
+++ src/components/FavCities.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+function TopButtons({ setQuery }) {
+  const cities = [
+    {
+      id: 1,
+      title: "Bangalore",
+    },
+    {
+      id: 2,
+      title: "Kathmandu",
+    },
+    {
+      id: 3,
+      title: "Hokkaido",
+    },
+    {
+      id: 4,
+      title: "Seoul",
+    },
+    {
+      id: 5,
+      title: "Kolkata",
+    },
+  ];
+
+  return (
+    <div className="flex items-center justify-around my-6">
+      {cities.map((city) => (
+        <button
+          key={city.id}
+          className="text-white text-lg font-medium"
+          onClick={() => setQuery({ q: city.title })}
+        >
+          {city.title}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default TopButtons;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export class Header extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+        <div className="flex flex-row justify-center my-6 ext-xl3 text-white font-light">
+            <h1>FAVORITE CITIES</h1>
+            </div>
+    )
+  }
+}
+
+export default Header
