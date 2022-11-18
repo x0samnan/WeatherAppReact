ad733f55be3e3ae56225e15952248b9833d77ce3
x0samnan <92310785+x0samnan@users.noreply.github.com>
2022-11-18 23:42:09 +0530

    Update weatherService.js

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,9 +1,7 @@
 import { DateTime } from "luxon";
 
-const API_KEY = "606894dcacb02d71c235f006f68e5c0e";
+const API_KEY = "606894dcacb02d71c235f006f68e5c0e"; //Use your API key here 
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
-
-// https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
 
 const getWeatherData = (infoType, searchParams) => {
   const url = new URL(BASE_URL + "/" + infoType);
